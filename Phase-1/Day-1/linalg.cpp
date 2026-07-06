6f7b3130b52af0d8a6b6ac6595bdd215d060649e
Your Name <your@email.com>
2026-07-06 15:40:25 -0700

    Implemented base structure of beginner linear algebra library

---
+++ Phase-1/Day-1/linalg.cpp
@@ -0,0 +1,21 @@
+#include "linalg.h"
+
+// A C++ raw implementation of the linear algebra library
+
+
+void add(int a, int b) {
+
+
+    
+}
+
+void sub(int a, int b) {}
+
+void scale(int a, int b) {}
+
+void dot(int a, int b) {}
+
+void length(int a) {}
+
+void magnitude(int a) {}
+
